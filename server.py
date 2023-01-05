539064060a9ed0dc6ab456a154609122310f3f38
jaa <jaa>
2023-01-04 18:36:42 -0800

    work

--- server.py
+++ server.py
@@ -1,49 +1,63 @@
 import socket
 import subprocess
+import os
+import glob
 
-def playpause():
+VIDDIR = "/home/jaa/Videos"
+
+def playpause(conn):
     subprocess.run(["playerctl", "play-pause"])
     print("playpause")
+    conn.sendall(bytes("playpause", "UTF-8"))
 
-def fastforward(sec=10):
+def fastforward(conn, sec=10):
     subprocess.run(["playerctl", "position", f"{sec}+"])
     print("ff")
+    conn.sendall(bytes("ff", "UTF-8"))
 
-def rewind(sec=10):
+def rewind(conn, sec=10):
     subprocess.run(["playerctl", "position", f"{sec}-"])
     print("rewind")
+    conn.sendall(bytes("rewind", "UTF-8"))
+
+def list_videos(conn):
+    files = []
+    files += list(glob.glob(VIDDIR + "/**/*.mp4", recursive=True))
+    files += list(glob.glob(VIDDIR + "/**/*.mkv", recursive=True))
+    msg = ",".join(files)
+    print(msg)
+    conn.sendall(bytes(msg, "UTF-8"))
 
 handlers = {
     "up": playpause,
     "down": playpause,
     "right": fastforward,
     "left": rewind,
-    "sel": playpause
+    "sel": list_videos
 }
 
-def handle(buf):
+def handle(buf, conn):
     buf = buf.decode()
-    handlers[buf]()
+    if buf in handlers:
+        handlers[buf](conn)
+    else:
+        conn.sendall(bytes("awaiting command...", "UTF-8"))
 
-HOST = ""
-PORT = 5000
-TIMEOUT = None
-MAXBUF = 256
+if __name__ == "__main__":
+    HOST = ""
+    PORT = 5000
+    TIMEOUT = None
+    MAXBUF = 2048
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.settimeout(TIMEOUT)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(TIMEOUT)
 
-s.bind((HOST, PORT))
-s.listen()
-print("Listening")
+    s.bind((HOST, PORT))
+    s.listen()
 
-while True:
-    conn, addr = s.accept()
-    conn.settimeout(TIMEOUT)
-    buf = conn.recv(MAXBUF)
-    if len(buf) == 0:
-        continue
-    print("Received", buf, "from", addr)
-    handle(buf)
-    print("==============")
-    conn.close()
+    while True:
+        conn, addr = s.accept()
+        conn.settimeout(TIMEOUT)
+        buf = conn.recv(MAXBUF)
+        resp = handle(buf, conn)
+        conn.close()
